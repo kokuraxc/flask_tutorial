3741fe5a0f993992fecb2b5ace6dcf1e7bcc523b
Guo Zhengjin <guo.zhengjin@gmail.com>
2019-12-14 01:13:20 +0800

    url_for, and HTTP methods

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-from html import escape
-
-from flask import Flask
+from flask import Flask, escape, url_for, request
 app = Flask(__name__)
 
 
@@ -25,4 +23,37 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
+
+
+@app.route('/projects/')
+def projects():
+    return 'the project page'
+
+
+@app.route('/about')
+def about():
+    return 'the about page'
+
+
+with app.test_request_context():
+    print(url_for('about'))
+    print(url_for('hello_world'))
+    print(url_for('show_subpath', subpath='this/is/sub/path'))
+    print(url_for('projects'))
+
+
+def show_the_login_form():
+    return 'Enter your login info'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+def do_the_login():
+    return 'You are loged in'
 
